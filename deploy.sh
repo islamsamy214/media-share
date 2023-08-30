fbc14bb59c65ae71209c1c760e07bc92d456cc01
islamsamy214 <islamsamy214@gmail.com>
2023-08-30 16:14:13 +0300

    depoly on render

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ composer update --no-dev --working-dir=/var/www/html
 
 echo "Running npm"
 npm install --prefix /var/www/html
+npm run prod --prefix /var/www/html
 
 echo "Caching config..."
 php artisan config:cache
